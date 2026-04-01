29993a2c401746d7ea1db5da85e10177bef321db
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-01 11:05:38 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,6 @@
+# Tuples in python are immutable sequences, typically used to store collections of heterogeneous data. They are defined using parentheses () and can contain elements of different types.
+
+
+numbers = (1, 2, 3, 4, 5); # defines a tuple of numbers
+print(numbers); # prints the entire tuple
+
