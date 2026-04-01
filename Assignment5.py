2491fce4a39c13e43d7bae148d4c14463a797658
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-01 10:45:00 +0530

    Conditional Statements

---
+++ Assignment5.py
@@ -0,0 +1,12 @@
+# Check whether a person ccan drive or not
+
+age = int(input("Enter your age: "));
+
+if age >= 18:
+    print("You are not a kid Brruhhh...")
+    print("You can drive");
+else:
+    print("HaHa... You are a kid")
+    print("You cannot drive");
+    print("Go and play with your toys");
+
