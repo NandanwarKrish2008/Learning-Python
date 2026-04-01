f60d7bb08bc3b8f761cf14856c167457bf43e233
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-01 11:01:24 +0530

    Update Assignment7.py

--- Assignment7.py
+++ Assignment7.py
@@ -5,7 +5,21 @@ num1 = float(input("Enter the first number: "));
 num2 = float(input("Enter the second number: "));
 num3 = float(input("Enter the third number: "));
 
-numbers = [num1, num2, num3];
+# numbers = [num1, num2, num3];
+# numbers.sort(); # sorts the list in ascending order
+# print("Sorted list: ", numbers);
+# print("Maximum number: ", max(numbers)); # finds the maximum number in the list
+# print("Minimum number: ", min(numbers)); # finds the minimum number in the list
+# print("Length of the list: ", len(numbers)); # finds the length of the list
+
+
+
+# USING APPEND METHOD
+
+numbers = [];
+numbers.append(num1);
+numbers.append(num2);
+numbers.append(num3);
 numbers.sort(); # sorts the list in ascending order
 print("Sorted list: ", numbers);
 print("Maximum number: ", max(numbers)); # finds the maximum number in the list
