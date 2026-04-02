daecd24a0e3082ce044981608596b64ab4200aa8
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-02 12:01:05 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+# A set is a collection of unordered and unique items. Sets automatically remove duplicate elements and are written using curly braces { }.
+
+
+languages = {"Python", "Java", "C++", "JavaScript", "Python"}; 
+
+print(languages); 
+
+empty_set = set(); 
+
+languages.add("Ruby"); 
+print(languages);
+
+languages.remove("Java"); 
+print(languages);
