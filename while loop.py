0bd3e16217fc8af6a3ec72f7576c3dcd6f181ef7
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-06 17:00:14 +0530

    Update while loop.py

--- while loop.py
+++ while loop.py
@@ -1,7 +1,13 @@
 # While Loop in python
 
-i = 1;
+# i = 1;
 
-while i<=10:
-    print(f"{i} is less than or equal to 10 ");
-    i += 1;
+# while i<=10:
+#     print(f"{i} is less than or equal to 10 ");
+#     i += 1;
+
+i = 10;
+
+while (i>=1):
+    print(f"{i} is greater than or equal to 1");
+    i -= 1;
