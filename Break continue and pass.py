851734271b3fceaeb9b7704a9faaecb343722781
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-06 20:18:48 +0530

    Create Break continue and pass.py

---
+++ Break continue and pass.py
@@ -0,0 +1,25 @@
+# BREAK FUNTION
+
+# for i in range(1, 11, 1):
+#     if i == 5:
+#         break;
+#     print(i);
+
+
+
+# CONTINUE FUNCTION
+
+# for i in range(1, 11, 1):
+#     if i == 5:
+#         continue;
+#     print(i);
+
+# PASS FUNCTION
+
+# Creates an empty loop
+
+for i in range(1, 11, 1):
+    if i == 5:
+        pass;
+    print(i);
+    
