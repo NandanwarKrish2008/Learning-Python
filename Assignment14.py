60117262c0b79515395abf50c9661983963447ba
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-06 19:58:17 +0530

    Create Assignment14.py

---
+++ Assignment14.py
@@ -0,0 +1,11 @@
+# Multiplication table of a number
+
+n = int(input("Enter a number : "));
+
+i = 1;
+
+while i <= 10:
+    print(f"{n} x {i} = {n * i}");
+    i += 1;
+
+print("Loop is ended here");
