db8637b54ef81ee63140efb04eee70cc208ec524
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-06 20:07:04 +0530

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,21 @@
+# for loop in python
+
+# list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+# for i in list:
+#     print(i);
+
+# tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+# for i in tuple:
+#     print(i);
+
+
+# RANGE FUNCTION
+# range(start, stop, step)
+# start default is 0
+# step default is 1
+# stop is exclusive
+
+for i in range(1, 11, 1):
+    print(i);
