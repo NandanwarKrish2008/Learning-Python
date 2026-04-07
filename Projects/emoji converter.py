16fb27a1d0b40ef9cc8316d6c388be393fd656f3
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-07 10:53:11 +0530

    Delete emoji converter.py

--- Projects/emoji converter.py
+++
@@ -1,7 +0,0 @@
-msg = input("Enter a message: ")
-
-msg = msg.replace(":)", "😊")
-msg = msg.replace(":(", "😞")
-msg = msg.replace(":D", "😃")
-msg = msg.replace(":/", "😕")
-print(f"Converted emoji : {msg}")
