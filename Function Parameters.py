b01ed4dc2719e477766d2ea623b91f3906106fb8
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-09 20:36:48 +0530

    Create Function Parameters.py

---
+++ Function Parameters.py
@@ -0,0 +1,8 @@
+def average(a, b):
+    avg = (a + b) / 2
+    print("The average is: ", avg)
+
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+average(x, y)
