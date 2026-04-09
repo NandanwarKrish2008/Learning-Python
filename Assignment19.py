7d2fc7a505526809a475db7b9cc9985d13d5c2f4
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-09 20:44:43 +0530

    Update Assignment19.py

---
+++ Assignment19.py
@@ -0,0 +1,10 @@
+# Print name with age
+
+def info(name, age):
+    print(f"{name} is {age} years old")
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+info(name, age)
