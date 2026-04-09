3aa5ee4dfd09d443960a2567ffe64f68a284dbdd
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-09 20:25:46 +0530

    Update Function.py

---
+++ Function.py
@@ -0,0 +1,16 @@
+# Functions in python
+
+# def hello():
+#     print("Hello World")
+#
+#
+# hello()
+
+
+def sumofnum():
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    c = a + b
+    print("The sum is: ", c)
+
+sumofnum()
