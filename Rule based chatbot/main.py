f63288d69724bd21a4b8692027eef25471e3878a
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-10 11:52:25 +0530

    Update main.py

--- Rule based chatbot/main.py
+++ Rule based chatbot/main.py
@@ -3,18 +3,30 @@ print("You can ask me basic questions and type 'bye' to exit the chat")
 
 # Chatbot memory creation
 responses = {
-    "Hello" : "Welcome! How may I help you?",
-    "How are you" : "I'm Fine. Thank you for asking!",
-    "Who are you" : "I'm a smart AI chatbot",
-    "Motivate me" : "Manzil ko bhulakar jiya toh kya jiya",
-    "Happy" : "Great to hear that"
+    "hello" : "Welcome! How may I help you?",
+    "how are you" : "I'm Fine. Thank you for asking!",
+    "who are you" : "I'm a smart AI chatbot",
+    "motivate me" : "Manzil ko bhulakar jiya toh kya jiya",
+    "happy" : "Great to hear that"
     }
 # Making function to get reply
 
-def getResponse(userQuestion):
-    userQuestion = userQuestion.lower()
-    
-# Take user input
+def getresponse(userquestion):
+    userquestion = userquestion.lower()
+    for eachkey in responses:
+        if eachkey in userquestion:
+            return responses[eachkey]
+        return "Sorry I didn't understand you"
 
-userinput = input("Please ask your question: ")
-reply = getResponse(userinput)
+    return "Program execution end"
+
+# Take user input
+while True:
+    userinput = input("Please ask your question: ")
+    reply = getresponse(userinput)
+    print(reply)
+
+    if reply.lower() == "bye":
+        break
+    else:
+        continue
