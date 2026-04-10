769e98779452a73d28042fa89466965cc8be2ce1
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-10 12:17:23 +0530

    File Handling

--- File Handling/filehandling.py
+++ File Handling/filehandling.py
@@ -1,4 +1,21 @@
-file = open("file.txt", "r")
-data = file.read()
+# Create file using python code
 
-print(data)
+# open("name.txt", "x")
+
+# Open file in read mode
+
+# file = open("file.txt", "r")
+# data = file.read()
+#
+# print(data)
+
+
+# Open file in write mode and write in it
+
+# file = open("file.txt", "w")
+# data = file.write("Hello World from File Handling")
+
+# Add text at last in file
+
+file = open("file.txt", "a")
+data = file.write("Python is amazing")
