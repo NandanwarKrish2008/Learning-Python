1ad136cea3cfc76198df424d772504492e50d40b
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-11 10:42:40 +0530

    Update FileReading.py

--- File Handling/FileReading.py
+++ File Handling/FileReading.py
@@ -1,4 +1,16 @@
-# With keyword
+# With keyword --> Closes file automatically
+
+# with open("file.txt", "r") as f:
+#     print(f.read())
 
 with open("file.txt", "r") as f:
-    print(f.read())
+    line1 = f.readline()
+    line2 = f.readline()
+    line3 = f.readline()
+    line4 = f.readline()
+    line5 = f.readline()
+    print(line1)
+    print(line2)
+    print(line3)
+    print(line4)
+    print(line5)
