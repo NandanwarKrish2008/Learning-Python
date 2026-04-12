7860b69800c5534a8c98b8f9a8f5610f410bb3ff
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-12 16:21:57 +0530

    OOPS

---
+++ Assignment22.py
@@ -0,0 +1,11 @@
+# Create class student that makes 3 marks and has method average()
+
+class Student:
+    def __init__(self, name, ListOfMarks):
+        self.name = name
+        self.ListOfMarks = ListOfMarks
+
+    def average():
+        pass
+
+student1 = Student("Krish", [99, 98, 97])
--- OOPs/init Constructor.py
+++ OOPs/init Constructor.py
@@ -1,9 +1,15 @@
 class Student:
     school_name = "XYZ High School"
-    def __init__(self):
-        print("Whenever the new object is created I'm called automatically")
-        print(self)
+    def __init__(self, name, course):
+        # print("Whenever the new object is created I'm called automatically")
+        # print(self)
+        self.name = name
+        self.course = course
+        print(self.name)
+        print(self.course)
 
+student1 = Student("Krish", "AI/ML") #init method will be called by default automatically
+# print(f"student1: {student1}")
 
-student1 = Student() #init method will be called by default automatically
-student2 = Student()
+student2 = Student("Rahul", "B.Tech")
+# print("student2: ", student2)
