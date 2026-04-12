ac7c33c8185df4b3cac745a6c40de073321e8f64
Nandanvar Krish <nandanwarkrish86@gmail.com>
2026-04-12 15:43:12 +0530

    Update class.py

---
+++ OOPs/class.py
@@ -0,0 +1,33 @@
+# Class creation
+class student:
+    name = "Krish" #Name, rollNo, std, etc are called attributes
+    rollNO = "25"
+    std = "12th"
+    stream = "pcm"
+
+# Object Creation
+
+# Student = student()
+# print(Student.name)
+# print(Student.rollNO)
+# print(Student.std)
+# print(Student.stream)
+
+
+Student1 = student()
+Student1.name = "Krish"
+Student1.rollNO = "HSC01"
+print(Student1.name)
+print(Student1.rollNO)
+
+Student2 = student()
+Student2.name = "Rakesh"
+Student2.rollNO = "HSC02"
+print(Student2.name)
+print(Student2.rollNO)
+
+Student3 = student()
+Student3.name = "Mukesh"
+Student3.rollNO = "HSC03"
+print(Student3.name)
+print(Student3.rollNO)
