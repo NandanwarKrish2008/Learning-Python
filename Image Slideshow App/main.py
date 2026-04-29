9fda75d23b2458a9ea3b7dc5bdaab2113dd5d106
“Nandanvar <nandanwarkrish86@gmail.com>
2026-04-29 21:06:55 +0530

    Update main.py

--- Image Slideshow App/main.py
+++ Image Slideshow App/main.py
@@ -10,3 +10,12 @@ root.geometry("1080x1920")
 
 # List of image path
 
+image = [
+    r"C:\Users\Dell\Desktop\Slideshow\image1.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image2.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image3.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image4.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image5.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image6.jpg",
+    r"C:\Users\Dell\Desktop\Slideshow\image7.jpg",
+]
