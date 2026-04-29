a19f884447fae97ec9df6b8eb88c5bdc619acb85
“Nandanvar <nandanwarkrish86@gmail.com>
2026-04-29 21:02:29 +0530

    Image Slideshow

--- Image Slideshow App/main.py
+++ Image Slideshow App/main.py
@@ -1,1 +1,12 @@
 import tkinter as tk
+import time
+from PIL import Image, ImageTk
+
+# Main Application Window
+
+root = tk.Tk()
+root.title("Image Slideshow")
+root.geometry("900x900")
+
+# List of image path
+
