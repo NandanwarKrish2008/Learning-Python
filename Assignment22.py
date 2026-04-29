ab90634a4b07b1ba599f7d2610dd254cc1e42bfa
“Nandanvar <nandanwarkrish86@gmail.com>
2026-04-29 19:17:19 +0530

    Update Assignment22.py

--- Assignment22.py
+++ Assignment22.py
@@ -5,7 +5,14 @@ class Student:
         self.name = name
         self.ListOfMarks = ListOfMarks
 
-    def average():
-        pass
+    def average(self):
+        sum = 0
+        for eachValue in self.ListOfMarks:
+            sum = sum + eachValue
+        
+        average = sum / 3
+        print(f"Average is {average}")
 
 student1 = Student("Krish", [99, 98, 97])
+
+student1.average()
